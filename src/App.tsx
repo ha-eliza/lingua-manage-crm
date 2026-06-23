eff10e2e74f70aa65b4ca7cd103774b4d7dcddb5
Елизавета <ha.eliza07@gmail.com>
2026-06-23 17:50:03 +0300

    Добавлен основной Layout и маршруты

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,124 @@
-import './App.css'
+import { Link, Outlet, useLocation } from "react-router";
+import "./App.css";
+import {
+  BookOutlined,
+  CalendarOutlined,
+  FolderOpenOutlined,
+  GroupOutlined,
+  LeftOutlined,
+  LogoutOutlined,
+  RightOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
+import { useAuthStore } from "./store/useAuthStore";
+import { message } from "antd";
 
 function App() {
+  const location = useLocation(); // для шапки, название адреса
+  const [isCollapsed, setIsCollapsed] = useState(false); // для выдвижения aside
+
+  // Забираем текущего пользователя и функцию выхода из Zustand
+  const currentUser = useAuthStore((state) => state.currentUser);
+  const logoutUser = useAuthStore((state) => state.logoutUser);
+
+  const userRole = currentUser?.role || "teacher";
+
+  const menuItems = [
+    { key: "/dashboard", icon: <UserOutlined />, label: "Мой кабинет" },
+    {
+      key: "/calendar",
+      icon: <CalendarOutlined />,
+      label: "Календарь",
+    },
+    {
+      key: "/materials",
+      icon: <FolderOpenOutlined />,
+      label: "Материалы",
+    },
+    {
+      key: "/students",
+      icon: <GroupOutlined />,
+      label: "Студенты",
+      onlyTeacher: true,
+    },
+    { key: "/notes", icon: <BookOutlined />, label: "Заметки" },
+    { key: "/login", icon: <LogoutOutlined />, label: "Выйти" },
+  ];
+
+  const allowedMenu = menuItems.filter(
+    (item) => !item.onlyTeacher || userRole === "teacher",
+  );
+
+  // Функция для перевода пути в понятный заголовок для шапки
+  const getPageTitle = (pathname: string) => {
+    // Ищем совпадение текущего пути в массиве меню преподавателя или студента
+    const allItems = [...menuItems];
+    const currentItem = allItems.find((item) => item.key === pathname);
+    return currentItem ? currentItem.label : "Панель управления";
+  };
 
   return (
-    <>
-    </>
-  )
+    <div className="layout">
+      <aside className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
+        <div className="logo">{isCollapsed ? "LM" : "LinguaManage"}</div>
+        <nav className="menu">
+          {allowedMenu.map((item) => {
+            const isActive = location.pathname === item.key;
+            const handleLinkClick = () => {
+              if (item.key === "/login") {
+                logoutUser(); // Очищаем стейт текущего юзера и роли в Zustand
+                message.success(`Вы вышли с аккаунта`);
+              }
+            };
+            return (
+              <Link
+                key={item.key}
+                to={item.key}
+                onClick={handleLinkClick}
+                className={`menu-link ${isActive ? "active" : ""}`}
+                title={isCollapsed ? item.label : ""} // Всплывающая подсказка при наведении в свернутом виде
+              >
+                {item.icon}
+                {/* Текст скрывается если есть класс collapsed */}
+                <span className="link-text">{item.label}</span>
+              </Link>
+            );
+          })}
+        </nav>
+
+        {/* isCollapsed: кнопка выдвижения бокового меню */}
+        <div
+          className="mt-auto mx-auto cursor-pointer toggle-btn"
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          {isCollapsed ? <RightOutlined /> : <LeftOutlined />}
+        </div>
+      </aside>
+
+      <div className="main-wrapper">
+        <header className="header">
+          <h2 className="text-white text-xl font-bold">
+            {/* название адреса */}
+            {getPageTitle(location.pathname)}
+          </h2>
+          <div
+            className="border-0 rounded-full text-white font-semibold py-1 px-2.5 cursor-pointer"
+            style={{
+              backgroundColor: currentUser?.avatarColor || "#1D3557", // Цвет из стора
+            }}
+          >
+            {/* Первая буква имени пользователя или L */}
+            {currentUser?.name ? currentUser.name.charAt(0).toUpperCase() : "L"}
+          </div>
+        </header>
+
+        <main className="m-5 bg-white rounded-xl h-full p-7">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,71 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { LoginPage } from "./pages/LoginPage.tsx";
+import { DashboardPage } from "./pages/Dashboard.tsx";
+import { CalendarPage } from "./pages/CalendarPage.tsx";
+import { StudentsPage } from "./pages/StudentsPage.tsx";
+import { MaterialsPage } from "./pages/MaterialsPage.tsx";
+import { NotesPage } from "./pages/NotesPage.tsx";
+import { StudentProfilePage } from "./pages/StudentProfilePage.tsx";
+import { ConfigProvider } from "antd";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    // Страница входа без layout
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    // Внутренняя часть системы
+    path: "/",
+    element: <App />, // Layout
+    children: [
+      {
+        path: "/dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/calendar",
+        element: <CalendarPage />,
+      },
+      {
+        path: "/students",
+        element: <StudentsPage />,
+      },
+      {
+        path: "/materials",
+        element: <MaterialsPage />,
+      },
+      {
+        path: "/notes",
+        element: <NotesPage />,
+      },
+      {
+        path: "/profile/:id",
+        element: <StudentProfilePage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <ConfigProvider // изменение стилей у AntD
+    theme={{
+      token: {
+        colorPrimary: "#1E3A8A",
+      },
+      components: {
+        Input: {
+          colorBorder: "#E2E8F0",
+        },
+      },
+    }}
+  >
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </ConfigProvider>,
+);
